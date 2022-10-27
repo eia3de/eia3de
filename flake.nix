5a2fec83420d83cfd96d758fc338df050035d9d2
ldesgoui <ldesgoui@gmail.com>
2022-10-28 00:00:39 +0200

    chore: use flake.parts

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     fenix = {
       url = "github:nix-community/fenix";
@@ -8,60 +10,41 @@
     };
   };
 
-  outputs = inputs @ { nixpkgs, ... }:
-    let
-      supportedSystems = [
+  outputs = { self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = [
         "x86_64-linux"
       ];
 
-      overlays = [
-        inputs.fenix.overlay
-      ];
+      perSystem = { lib, pkgs, inputs', ... }:
+        let
+          inherit (pkgs) pkgsBuildHost;
+        in
+        {
+          devShells.default = pkgs.mkShell rec  {
+            nativeBuildInputs = with pkgsBuildHost; [
+              inputs'.fenix.packages.default.toolchain
 
-      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
-      withPkgsForEachSystem = f: forEachSystem (system: f (import nixpkgs { inherit system overlays; }));
-    in
-    {
-      devShell = withPkgsForEachSystem (pkgs: pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          pkgconfig
-          clang
-          lld
-        ];
-
-        buildInputs = with pkgs; [
-          udev
-          alsaLib
-          vulkan-loader
-          x11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-          libxkbcommon
-          wayland
-
-          cargo-watch
-
-          fenix.default.toolchain
-        ];
-
-        shellHook =
-          let
-            prepend = with pkgs; lib.makeLibraryPath [
-              udev
-              alsaLib
-              vulkan-loader
-              libxkbcommon
-              wayland
+              pkg-config
             ];
-          in
-          ''
-            export LD_LIBRARY_PATH="${prepend}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-          '';
 
-        # TODO: system -> triple mapping
-        CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "clang";
-        CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-C link-arg=-fuse-ld=lld";
-      });
+            buildInputs = with pkgs; [
+              alsaLib
+              libxkbcommon
+              udev
+              vulkan-loader
+              wayland
+              xlibsWrapper
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+            ];
+
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+            CARGO_TARGET_LINKER = "${pkgsBuildHost.clang}/bin/clang";
+            CARGO_TARGET_RUSTFLAGS = "-C link-arg=-fuse-ld=${lib.getExe pkgsBuildHost.mold} -Z share-generics=y";
+          };
+        };
     };
 }
