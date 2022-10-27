ceb41433e46e13e8b374732a2659acea3e54601a
ldesgoui <ldesgoui@gmail.com>
2022-10-28 00:12:40 +0200

    feat: run in windowed fullscreen with no vsync

--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -3,6 +3,11 @@ use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            mode: bevy::window::WindowMode::BorderlessFullscreen,
+            present_mode: bevy::window::PresentMode::AutoNoVsync,
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
