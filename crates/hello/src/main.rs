92ecdfc72409eedff04638ec684a7d7831a65c30
ldesgoui <ldesgoui@gmail.com>
2022-11-10 19:20:00 +0100

    feat: build a few entities

--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -11,5 +11,38 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
+        .add_startup_system(my_setup)
         .run();
+}
+
+// Marker, entity is the player
+#[derive(Component)]
+struct MyPlayer;
+
+// Marker, entity is the head of the player entity
+#[derive(Component)]
+struct MyPlayerHead;
+
+fn my_setup(mut cmd: Commands) {
+    cmd.spawn()
+        .insert(MyPlayer)
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0., 2., 0.)))
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::cylinder(1., 0.5))
+        .insert(KinematicCharacterController { ..default() })
+        .with_children(|parent| {
+            parent
+                .spawn()
+                .insert(MyPlayerHead)
+                .insert_bundle(Camera3dBundle {
+                    transform: Transform::from_xyz(0., 0.75, 0.),
+                    // transform: Transform::from_xyz(3., 0.75, 3.).looking_at(Vec3::ZERO, Vec3::Y),
+                    ..default()
+                });
+        });
+
+    cmd.spawn()
+        .insert(RigidBody::Fixed)
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0., -1., 0.)))
+        .insert(Collider::cuboid(100., 1., 100.));
 }
