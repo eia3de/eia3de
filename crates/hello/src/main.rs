7d3c7721eb6c2be383ccbc4eabe89132ebd90efa
ldesgoui <ldesgoui@gmail.com>
2022-11-14 01:21:32 +0100

    chore: update to bevy 0.9

--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -3,12 +3,14 @@ use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            mode: bevy::window::WindowMode::BorderlessFullscreen,
-            present_mode: bevy::window::PresentMode::AutoNoVsync,
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                mode: bevy::window::WindowMode::BorderlessFullscreen,
+                present_mode: bevy::window::PresentMode::AutoNoVsync,
+                ..default()
+            },
             ..default()
-        })
-        .add_plugins(DefaultPlugins)
+        }))
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(my_setup)
@@ -24,25 +26,26 @@ struct MyPlayer;
 struct MyPlayerHead;
 
 fn my_setup(mut cmd: Commands) {
-    cmd.spawn()
-        .insert(MyPlayer)
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0., 2., 0.)))
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Collider::cylinder(1., 0.5))
-        .insert(KinematicCharacterController { ..default() })
-        .with_children(|parent| {
-            parent
-                .spawn()
-                .insert(MyPlayerHead)
-                .insert_bundle(Camera3dBundle {
-                    transform: Transform::from_xyz(0., 0.75, 0.),
-                    // transform: Transform::from_xyz(3., 0.75, 3.).looking_at(Vec3::ZERO, Vec3::Y),
-                    ..default()
-                });
-        });
+    cmd.spawn((
+        MyPlayer,
+        TransformBundle::from(Transform::from_xyz(0., 2., 0.)),
+        RigidBody::KinematicPositionBased,
+        Collider::cylinder(1., 0.5),
+        KinematicCharacterController { ..default() },
+    ))
+    .with_children(|parent| {
+        parent.spawn((
+            MyPlayerHead,
+            Camera3dBundle {
+                transform: Transform::from_xyz(0., 0.75, 0.),
+                ..default()
+            },
+        ));
+    });
 
-    cmd.spawn()
-        .insert(RigidBody::Fixed)
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0., -1., 0.)))
-        .insert(Collider::cuboid(100., 1., 100.));
+    cmd.spawn((
+        RigidBody::Fixed,
+        TransformBundle::from(Transform::from_xyz(0., -1e-5, 0.)),
+        Collider::cuboid(1e5, 1e-5, 1e5),
+    ));
 }
